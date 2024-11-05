4751d31ceff23f6f44908aefbcbeb1ec4df5fbaa
hodoantrang12 <doantranghothi2005@gmail.com>
2024-11-06 04:08:41 +0700

    Add files via upload

---
+++ Session_05.cs
@@ -0,0 +1,18 @@
+﻿internal class Functions
+{
+    public static void swap(ref int a, ref int b)
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    public static void Main(string[] args)
+    {
+        int a = 6, b = 7;
+        Console.WriteLine($"Before call a={a}, b={b}");
+        swap(ref a, ref b);
+        Console.WriteLine($"After call a={a}, b={b}");
+
+    }
+}
